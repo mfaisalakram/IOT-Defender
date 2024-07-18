952e4f7c3d3908210242a0411a97afdf3f7d7fff
faisalakram <faisalakram1133@gmial.com>
2024-07-18 23:27:17 +0500

    banner and client section structure is complete

--- src/component/Banner/Banner.js
+++ src/component/Banner/Banner.js
@@ -1,10 +1,34 @@
 import React from "react";
 
+import banner_gif from "../../assets/gif/banner-gif.gif";
+
+import "./Banner.css";
+
 export const Banner = () => {
   return (
-    <div className="d-flex">
-      <div>d</div>
-      <div>e</div>
+    <div className="Banner">
+      <div className="global-container">
+        <div className="brand-content">
+          <h1 className="banner-heading">Revolutionize IoT Data Security</h1>
+          <p className="banner-text">
+            <span className="highlight-text">Empower your</span> IoT solutions
+            <span className="highlight-text">with decentralized</span>{" "}
+            blockchain security. Transparent,{" "}
+            <span className="highlight-text">Immutable, and</span>Scalable.
+          </p>
+          <div className="button-block">
+            <button className="book-demo-button button_primary">
+              Book a demo
+            </button>
+            <button className="learn-more-button button_secondary">
+              Learn More
+            </button>
+          </div>
+        </div>
+        <div className="gif-container">
+          <img src={banner_gif} />
+        </div>
+      </div>
     </div>
   );
 };
--- src/component/Clients/Clients.js
+++ src/component/Clients/Clients.js
@@ -1,5 +1,54 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Swiper from "swiper";
+import "swiper/css";
+
+import amazone from "../../assets/client_logos/amazone.png";
+import frame from "../../assets/client_logos/frame.png";
+import google_cloud from "../../assets/client_logos/google-cloud.png";
+import hyperledger from "../../assets/client_logos/hyperledger.png";
+import microsoft_azure from "../../assets/client_logos/microsoft-azure.png";
+import s_software from "../../assets/client_logos/s-software.png";
+
+import "./Clients.css";
 
 export const Clients = () => {
-  return <div>Clients</div>;
+  useEffect(() => {
+    var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 6,
+      autoplay: {
+        delay: 1500,
+      },
+      loop: true,
+      spaceBetween: 10,
+    });
+  }, []);
+  return (
+    <div className="Clients">
+      <div classNameName="global-container">
+        <div className="swiper mySwiper">
+          <div className="swiper-wrapper">
+            <div className="swiper-slide">
+              <img src={hyperledger} />
+            </div>
+            <div className="swiper-slide">
+              <img src={s_software} />
+            </div>
+            <div className="swiper-slide">
+              <img src={amazone} />
+            </div>
+            <div className="swiper-slide">
+              <img src={microsoft_azure} />
+            </div>
+            <div className="swiper-slide">
+              <img src={google_cloud} />
+            </div>
+            <div className="swiper-slide">
+              <img src={frame} />
+            </div>
+          </div>
+          <div className="swiper-pagination"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
