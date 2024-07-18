7784a1dae9278c25034e29cb42d819aededd54a3
faisalakram <faisalakram1133@gmial.com>
2024-07-18 21:52:21 +0500

    bootstrap cdn added

--- src/component/Banner/Banner.js
+++ src/component/Banner/Banner.js
@@ -1,5 +1,10 @@
 import React from "react";
 
 export const Banner = () => {
-  return <div>Banner</div>;
+  return (
+    <div className="d-flex">
+      <div>d</div>
+      <div>e</div>
+    </div>
+  );
 };
