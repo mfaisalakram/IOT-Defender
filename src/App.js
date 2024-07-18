b6f0c772f0abecfeeb7957ebbf5f957085f8a448
faisalakram <faisalakram1133@gmial.com>
2024-07-18 14:21:17 +0500

    section setup done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
+import { Banner } from "./component/Banner/Banner";
+import { Clients } from "./component/Clients/Clients";
+import { Footer } from "./component/common/Footer/Footer";
+import { Header } from "./component/common/Header/Header";
+import { Featured } from "./component/Featured/Featured";
+import { HowItWork } from "./component/HowItWork/HowItWork";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+
+      <Banner />
+      <Clients />
+      <Featured />
+      <HowItWork />
+
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
