ec38395ab490298af3bb6b12e16a9c402769592f
Ali-Raza-400 <aliraza.12748g@gmail.com>
2024-07-18 22:55:39 +0500

    make  header and footer in progress

--- src/component/common/Footer/Footer.js
+++ src/component/common/Footer/Footer.js
@@ -1,5 +1,28 @@
+import { Box, Typography } from "@mui/material";
 import React from "react";
 
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Box className="global-container">
+      <Box
+        className="logo"
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          width: "100%",
+          // background: "red",
+          flex: 1,
+        }}
+      >
+        <img src="/assets/logo.png" alt="" width="200px" />
+        <Typography sx={{ lineHeight: "24px", fontWeight: "400" }}>
+          Empower your IoT solutions with decentralized blockchain security.
+          Transparent, Immutable, and Scalable.
+        </Typography>
+        <Typography sx={{ lineHeight: "16px", fontWeight: "400" }}>
+          support@wolfberryllc.com
+        </Typography>
+      </Box>
+    </Box>
+  );
 };
